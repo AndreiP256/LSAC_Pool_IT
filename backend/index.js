4cb74d2aa802d554ea48a09ede831d7e3db51e78
AndreiP256 <andreiprusacov@gmail.com>
2023-11-23 21:48:09 +0200

    I got the backend get ALL users request working

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router = express.Router();
+const app = express();
 
-const app = express()
+mongoose.connect('mongodb+srv://Prusi-admin:Database_pass@lsac-poll-it.g9u2hcc.mongodb.net/poll_it_app')
+.then(() => {
+    console.log("Database Connected");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
 
-mongoose.connect('mongodb://localhost:27017/pool_it')
+const UserSchema = mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const UserModel = mongoose.model("User", UserSchema)
+
+
+app.get("/users", (req, res) =>{
+    UserModel.find({})
+        .then(function(users){
+            console.log("i got a user")
+            res.json(users)
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+    })
+
+app.get("/new_user")
+
 
 app.listen(5000, () => {
     console.log("Server is running")
