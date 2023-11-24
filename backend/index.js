f31774eda92b98cb23fb852b0e9baa7158c28df5
AndreiP256 <andreiprusacov@gmail.com>
2023-11-24 13:17:25 +0200

    The program now checks for email allready  in use

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const router = express.Router();
 const app = express();
+
+
 
 mongoose.connect('mongodb+srv://Prusi-admin:Database_pass@lsac-poll-it.g9u2hcc.mongodb.net/poll_it_app')
 .then(() => {
@@ -18,6 +21,7 @@ const UserSchema = mongoose.Schema({
 
 const UserModel = mongoose.model("User", UserSchema)
 
+app.use(bodyParser.json());
 
 app.get("/users", (req, res) =>{
     UserModel.find({})
@@ -30,21 +34,32 @@ app.get("/users", (req, res) =>{
         })
     })
 
-app.use("/new", (req, res) => {
-    const user = new UserModel({
-        email: req.body.email,
-        password: req.body.password
-    })
-    user.save()
-        .then(function(){
-            console.log("User saved")
-            res.json(user)
-        })
-        .catch(function(err){
-            console.log(err)
-        })
-})
-
+    app.post("/new", (req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+    
+        UserModel.findOne({ email: email })
+            .then(user => {
+                if (user) {
+                    res.status(400).json({ message: "Email already in use" });
+                } else {
+                    const newUser = new UserModel({ email, password });
+                    newUser.save()
+                        .then(() => {
+                            console.log("User saved");
+                            res.json(newUser);
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({ message: "Error saving user" });
+                        });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ message: "Error checking user existence" });
+            });
+    });
 
 app.listen(5000, () => {
     console.log("Server is running")
