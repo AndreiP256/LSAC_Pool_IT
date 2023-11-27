272eb493ce66c97bda5ee052e72d7427bf0cb66a
AndreiP256 <andreiprusacov@gmail.com>
2023-11-27 11:34:35 +0200

    Made error 404

--- frontend/src/pages/error404/error404.js
+++ frontend/src/pages/error404/error404.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import "./error404.css"
+
 export default function Error404() {
     return (
-        <div>
-            Home
+        <div className = "error-page" style={{ textAlign: 'center', marginTop: '20vh' }}>
+            <h1>404</h1>
+            <p>Oops! The page you are looking for does not exist.</p>
+            <a href="/">Go back to Home</a>
         </div>
-    )
+    );
 }
